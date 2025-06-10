8f8fb852191d329a7fa8ca6525825e10facfb927
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-10 11:14:14 +0530

    Add files via upload
    javafundamentals

---
+++ fifth.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class fifth
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double 	radius=sc.nextDouble();
+		double 	height=sc.nextDouble();
+		double Volume = 3.14*radius*radius*height;
+		System.out.print(Volume);
+
+	}
+}
