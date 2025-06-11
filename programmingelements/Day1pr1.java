04cf3b8efe3d2b07feaf928e935d65834b9bd9b5
Himanshu-Raghav <himanshud310@gmail.com>
2025-06-11 09:39:40 +0530

    DAY1PROBLEM1

---
+++ programmingelements/Day1pr1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Day1pr1
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int year=sc.nextInt();
+	    int age=2024-year;
+	    System.out.print(age);
+		
+	}
+}		
